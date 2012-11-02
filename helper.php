bce91519a3d5241686df2dfbfa1e82e88d4c4607
Todor Iliev <todor@itprism.com>
2012-11-02 16:53:56 +0200

    fixed mail url

--- helper.php
+++ helper.php
@@ -101,6 +101,8 @@ class ItpSocialButtonsHelper{
         
         JLoader::register("MailToHelper", JPATH_SITE . '/components/com_mailto/helpers/mailto.php');
         
+        $link     = rawurldecode($link);
+        
 		$template = JFactory::getApplication()->getTemplate();
 		$url	  = 'index.php?option=com_mailto&tmpl=component&template='.$template.'&link='.MailToHelper::addLink($link);
 

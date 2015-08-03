cd7b030bd95ef06679304b057f2921d7898f57bf
Oleg <papagot@w-services.eu>
2015-08-03 10:07:05 +0300

    Add VK button
    Not all images ONLY SMALL

--- helper.php
+++ helper.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package      ITPSocialButtons
  * @subpackage   Modules
@@ -6,12 +7,11 @@
  * @copyright    Copyright (C) 2015 Todor Iliev <todor@itprism.com>. All rights reserved.
  * @license      http://www.gnu.org/copyleft/gpl.html GNU/GPL
  */
-
 // no direct access
 defined('_JEXEC') or die;
 
-class ItpSocialButtonsHelper
-{
+class ItpSocialButtonsHelper {
+
     /**
      * A method that make a long url to short url
      *
@@ -20,12 +20,11 @@ class ItpSocialButtonsHelper
      *
      * @return string
      */
-    public static function getShortUrl($link, $params)
-    {
+    public static function getShortUrl($link, $params) {
         JLoader::register("ItpSocialButtonsModuleShortUrl", dirname(__FILE__) . DIRECTORY_SEPARATOR . "shorturl.php");
 
         $options = array(
-            "login"   => $params->get("login"),
+            "login" => $params->get("login"),
             "api_key" => $params->get("apiKey"),
             "service" => $params->get("shortUrlService"),
         );
@@ -33,14 +32,13 @@ class ItpSocialButtonsHelper
         $shortLink = "";
         try {
 
-            $shortUrl  = new ItpSocialButtonsModuleShortUrl($link, $options);
+            $shortUrl = new ItpSocialButtonsModuleShortUrl($link, $options);
             $shortLink = $shortUrl->getUrl();
 
             // Get original link
             if (!$shortLink) {
                 $shortLink = $link;
             }
-
         } catch (Exception $e) {
 
             JLog::add($e->getMessage(), JLog::DEBUG);
@@ -49,7 +47,6 @@ class ItpSocialButtonsHelper
             if (!$shortLink) {
                 $shortLink = $link;
             }
-
         }
 
         return $shortLink;
@@ -65,12 +62,11 @@ class ItpSocialButtonsHelper
      *
      * @return string
      */
-    public static function getExtraButtons($title, $url, &$params)
-    {
+    public static function getExtraButtons($title, $url, &$params) {
         $html = "";
         // Extra buttons
         for ($i = 1; $i < 6; $i++) {
-            $btnName     = "ebuttons" . $i;
+            $btnName = "ebuttons" . $i;
             $extraButton = $params->get($btnName, "");
             if (!empty($extraButton)) {
 
@@ -113,19 +109,18 @@ class ItpSocialButtonsHelper
      *
      * @return string
      */
-    public static function sendToFriendIcon($imageSrc, $link)
-    {
+    public static function sendToFriendIcon($imageSrc, $link) {
         JLoader::register("MailToHelper", JPATH_SITE . '/components/com_mailto/helpers/mailto.php');
 
         $link = rawurldecode($link);
 
         $template = JFactory::getApplication()->getTemplate();
-        $url      = 'index.php?option=com_mailto&tmpl=component&template=' . $template . '&link=' . MailToHelper::addLink($link);
+        $url = 'index.php?option=com_mailto&tmpl=component&template=' . $template . '&link=' . MailToHelper::addLink($link);
 
         $status = 'width=400,height=350,menubar=yes,resizable=yes';
 
         $attribs = array(
-            'title'   => JText::_('JGLOBAL_EMAIL'),
+            'title' => JText::_('JGLOBAL_EMAIL'),
             'onclick' => "window.open(this.href,'win2','" . $status . "'); return false;"
         );
 
@@ -143,8 +138,7 @@ class ItpSocialButtonsHelper
      *
      * @return string
      */
-    private static function printIconLink($imageSrc)
-    {
+    private static function printIconLink($imageSrc) {
         $icon = '<img src="' . $imageSrc . '" alt="' . JText::_('MOD_ITPSOCIALBUTTONS_PRINT_THIS_PAGE') . '" title="' . JText::_('MOD_ITPSOCIALBUTTONS_PRINT_THIS_PAGE') . '" />';
 
         return '<a href="#" onclick="window.print();return false;">' . $icon . '</a>';
@@ -158,8 +152,7 @@ class ItpSocialButtonsHelper
      *
      * @return string
      */
-    private static function printIconPopup($imageSrc, $link)
-    {
+    private static function printIconPopup($imageSrc, $link) {
         $app = JFactory::getApplication();
 
         $link = rawurldecode($link);
@@ -167,18 +160,18 @@ class ItpSocialButtonsHelper
         // Set the number of page.
         $page = "";
         if ($app->input->getInt("limitstart")) {
-            $page = "&page=".$app->input->getInt("limitstart");
+            $page = "&page=" . $app->input->getInt("limitstart");
         }
 
         // Generate an URL
-        $url  = $link . '?tmpl=component&print=1&layout=default' . $page;
+        $url = $link . '?tmpl=component&print=1&layout=default' . $page;
 
         $status = 'status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no';
 
         $attributes = array(
-            'title'   => JText::_('JGLOBAL_PRINT'),
+            'title' => JText::_('JGLOBAL_PRINT'),
             'onclick' => "window.open(this.href,'win2','" . $status . "'); return false;",
-            'rel'     => 'nofollow'
+            'rel' => 'nofollow'
         );
 
         $text = '<img src="' . $imageSrc . '" alt="' . JText::_('MOD_ITPSOCIALBUTTONS_PRINT_THIS_PAGE') . '" title="' . JText::_('MOD_ITPSOCIALBUTTONS_PRINT_THIS_PAGE') . '" />';
@@ -186,59 +179,58 @@ class ItpSocialButtonsHelper
         return JHtml::_('link', $url, $text, $attributes);
     }
 
-    public static function getDeliciousButton($title, $link, $style)
-    {
+    public static function getDeliciousButton($title, $link, $style) {
         $img_url = $style . "/delicious.png";
 
         return '<a href="http://del.icio.us/post?url=' . $link . '&amp;title=' . $title . '" title="' . JText::sprintf("MOD_ITPSOCIALBUTTONS_SUBMIT", "Delicious") . '" target="_blank" ><img src="' . $img_url . '" alt="' . JText::sprintf("MOD_ITPSOCIALBUTTONS_SUBMIT", "Delicious") . '" /></a>';
     }
 
-    public static function getDiggButton($title, $link, $style)
-    {
+    public static function getDiggButton($title, $link, $style) {
         $img_url = $style . "/digg.png";
 
         return '<a href="http://digg.com/submit?url=' . $link . '&amp;title=' . $title . '" title="' . JText::sprintf("MOD_ITPSOCIALBUTTONS_SUBMIT", "Digg") . '" target="_blank" ><img src="' . $img_url . '" alt="' . JText::sprintf("MOD_ITPSOCIALBUTTONS_SUBMIT", "Digg") . '" /></a>';
     }
 
-    public static function getFacebookButton($title, $link, $style)
-    {
+    public static function getFacebookButton($title, $link, $style) {
         $img_url = $style . "/facebook.png";
 
         return '<a href="http://www.facebook.com/sharer.php?u=' . $link . '&amp;t=' . $title . '" title="' . JText::sprintf("MOD_ITPSOCIALBUTTONS_SUBMIT", "Facebook") . '" target="_blank" ><img src="' . $img_url . '" alt="' . JText::sprintf("MOD_ITPSOCIALBUTTONS_SUBMIT", "Facebook") . '" /></a>';
     }
 
-    public static function getGoogleButton($link, $style)
-    {
+    public static function getGoogleButton($link, $style) {
         $img_url = $style . "/google.png";
 
         return '<a href="https://plus.google.com/share?url=' . $link . '" title="' . JText::sprintf("MOD_ITPSOCIALBUTTONS_SUBMIT", "Google Plus") . '" target="_blank" ><img src="' . $img_url . '" alt="' . JText::sprintf("MOD_ITPSOCIALBUTTONS_SUBMIT", "Google Plus") . '" /></a>';
     }
 
-    public static function getStumbleuponButton($title, $link, $style)
-    {
+    public static function getStumbleuponButton($title, $link, $style) {
         $img_url = $style . "/stumbleupon.png";
 
         return '<a href="http://www.stumbleupon.com/submit?url=' . $link . '&amp;title=' . $title . '" title="' . JText::sprintf("MOD_ITPSOCIALBUTTONS_SUBMIT", "Stumbleupon") . '" target="_blank" ><img src="' . $img_url . '" alt="' . JText::sprintf("MOD_ITPSOCIALBUTTONS_SUBMIT", "Stumbleupon") . '" /></a>';
     }
 
-    public static function getTechnoratiButton($link, $style)
-    {
+    public static function getTechnoratiButton($link, $style) {
         $img_url = $style . "/technorati.png";
 
         return '<a href="http://technorati.com/faves?add=' . $link . '" title="' . JText::sprintf("MOD_ITPSOCIALBUTTONS_SUBMIT", "Technorati") . '" target="_blank" ><img src="' . $img_url . '" alt="' . JText::sprintf("MOD_ITPSOCIALBUTTONS_SUBMIT", "Technorati") . '" /></a>';
     }
 
-    public static function getTwitterButton($title, $link, $style)
-    {
+    public static function getTwitterButton($title, $link, $style) {
         $img_url = $style . "/twitter.png";
 
         return '<a href="http://twitter.com/share?text=' . $title . "&amp;url=" . $link . '" title="' . JText::sprintf("MOD_ITPSOCIALBUTTONS_SUBMIT", "Twitter") . '" target="_blank" ><img src="' . $img_url . '" alt="' . JText::sprintf("MOD_ITPSOCIALBUTTONS_SUBMIT", "Twitter") . '" /></a>';
     }
 
-    public static function getLinkedInButton($title, $link, $style)
-    {
+    public static function getLinkedInButton($title, $link, $style) {
         $img_url = $style . "/linkedin.png";
 
         return '<a href="http://www.linkedin.com/shareArticle?mini=true&amp;url=' . $link . '&amp;title=' . $title . '" title="' . JText::sprintf("MOD_ITPSOCIALBUTTONS_SUBMIT", "LinkedIn") . '" target="_blank" ><img src="' . $img_url . '" alt="' . JText::sprintf("MOD_ITPSOCIALBUTTONS_SUBMIT", "LinkedIn") . '" /></a>';
     }
+
+    public static function getVKButton($title, $link, $style) {
+        $img_url = $style . "/VK.png";
+
+        return '<a href="http://vk.com/share.php?url=' . $link . '&amp;title=' . $title . '" title="' . JText::sprintf("MOD_ITPSOCIALBUTTONS_SUBMIT", "ВК") . '" target="_blank" ><img src="' . $img_url . '" alt="' . JText::sprintf("MOD_ITPSOCIALBUTTONS_SUBMIT", "BK") . '" /></a>';
+    }
+
 }
--- tmpl/default.php
+++ tmpl/default.php
@@ -6,44 +6,46 @@
  * @copyright    Copyright (C) 2015 Todor Iliev <todor@itprism.com>. All rights reserved.
  * @license      http://www.gnu.org/copyleft/gpl.html GNU/GPL
  */
-
 // no direct access
-defined( "_JEXEC" ) or die;
+defined("_JEXEC") or die;
 ?>
-<div class="itp-socialbuttons-mod<?php echo $moduleclass_sfx;?>">
-   <?php if($params->get('showTitle')){ ?>
-   <h4><?php echo $params->get('title');?></h4>
-   <?php }?>
-   
-   <div class="<?php echo $params->get('displayLines');?>">
-        <div class="<?php echo $params->get('displayIcons');?>">
-        <?php 
-        if($params->get("displayDelicious")) {
-          echo ItpSocialButtonsHelper::getDeliciousButton($title, $link, $stylePath);
-        }
-        if($params->get("displayDigg")) {
-            echo ItpSocialButtonsHelper::getDiggButton($title, $link, $stylePath);
-        }
-        if($params->get("displayFacebook")) {
-            echo ItpSocialButtonsHelper::getFacebookButton($title, $link, $stylePath);
-        }
-        if($params->get("displayGoogle")) {
-            echo ItpSocialButtonsHelper::getGoogleButton($link, $stylePath);
-        }
-        if($params->get("displaySumbleUpon")) {
-            echo ItpSocialButtonsHelper::getStumbleuponButton($title, $baseLink, $stylePath);
-        }
-        if($params->get("displayTechnorati")) {
-            echo ItpSocialButtonsHelper::getTechnoratiButton($link, $stylePath);
-        }
-        if($params->get("displayTwitter")) {
-            echo ItpSocialButtonsHelper::getTwitterButton($title, $link, $stylePath);
-        }
-        if($params->get("displayLinkedIn")) {
-            echo ItpSocialButtonsHelper::getLinkedInButton($title, $link, $stylePath);
-        }
-        ?>
-        <?php echo ItpSocialButtonsHelper::getExtraButtons($title, $link, $params); ?>
+<div class="itp-socialbuttons-mod<?php echo $moduleclass_sfx; ?>">
+    <?php if ($params->get('showTitle')) { ?>
+        <h4><?php echo $params->get('title'); ?></h4>
+    <?php } ?>
+
+    <div class="<?php echo $params->get('displayLines'); ?>">
+        <div class="<?php echo $params->get('displayIcons'); ?>">
+            <?php
+            if ($params->get("displayDelicious")) {
+                echo ItpSocialButtonsHelper::getDeliciousButton($title, $link, $stylePath);
+            }
+            if ($params->get("displayDigg")) {
+                echo ItpSocialButtonsHelper::getDiggButton($title, $link, $stylePath);
+            }
+            if ($params->get("displayFacebook")) {
+                echo ItpSocialButtonsHelper::getFacebookButton($title, $link, $stylePath);
+            }
+            if ($params->get("displayGoogle")) {
+                echo ItpSocialButtonsHelper::getGoogleButton($link, $stylePath);
+            }
+            if ($params->get("displaySumbleUpon")) {
+                echo ItpSocialButtonsHelper::getStumbleuponButton($title, $baseLink, $stylePath);
+            }
+            if ($params->get("displayTechnorati")) {
+                echo ItpSocialButtonsHelper::getTechnoratiButton($link, $stylePath);
+            }
+            if ($params->get("displayTwitter")) {
+                echo ItpSocialButtonsHelper::getTwitterButton($title, $link, $stylePath);
+            }
+            if ($params->get("displayLinkedIn")) {
+                echo ItpSocialButtonsHelper::getLinkedInButton($title, $link, $stylePath);
+            }
+            if ($params->get("displayVK")) {
+                echo ItpSocialButtonsHelper::getVKButton($title, $link, $stylePath);
+            }
+            ?>
+            <?php echo ItpSocialButtonsHelper::getExtraButtons($title, $link, $params); ?>
         </div>
-   </div>
+    </div>
 </div>
